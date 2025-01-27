c15416f7593b78f9595d7de5c647d5af688088b2
colix <148950843+YeahhColix@users.noreply.github.com>
2025-01-28 06:59:16 +0800

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -76,9 +76,6 @@ export default {
                             <td class="mobile">
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
-                            <td class="hz">
-                                <p>{{ record.hz }}Hz</p>
-                            </td>
                         </tr>
                     </table>
                 </div>
